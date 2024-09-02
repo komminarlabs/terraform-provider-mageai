127e25fe275cc3ecf1aa18c429f4c0272ec8454a
Thulasiraj Komminar <39799163+thulasirajkomminar@users.noreply.github.com>
2024-09-02 22:09:29 +0200

    match schema to api

--- internal/provider/block_resource.go
+++ internal/provider/block_resource.go
@@ -9,6 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/setdefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
@@ -194,8 +195,8 @@ func (r *BlockResource) Schema(ctx context.Context, req resource.SchemaRequest, 
 			},
 			"upstream_blocks": schema.SetAttribute{
 				Computed:    true,
-				Optional:    true,
 				Description: "The block UUIDs that this block depends on.",
+				Default:     setdefault.StaticValue(types.SetUnknown(types.StringType)),
 				ElementType: types.StringType,
 			},
 			"uuid": schema.StringAttribute{
